d462a6609be2fbcf38c14e0a0cc3dd5f427f096a
DriesSwinnenPXL <dries.swinnen@pxl.be>
2019-12-09 08:25:50 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,504 @@
+let data = `
+[
+  {
+    "contact": "week 1 - Angular",
+    "content": [
+      {
+        "section": "H0 Intro web expert",
+        "description": "Dit hoofdstuk geeft een algemeen beeld van het vak Web Expert.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Intro web expert",
+              "linkUrl": "https://docs.google.com/presentation/d/1q3SnLGD7DUw7pr_pGHcifu3DoNme_rBGLYsSlNGO5Rs/edit?usp=sharing"
+            },
+          {
+            "tag": "studiewijzer",
+            "linkText":"Moduleboek",
+            "linkUrl": "https://docs.google.com/document/d/1fOOXM5Anz-Mo48Js8jRpjl4LENsSIhZS8AX5lxKBkps/edit?usp=sharing"
+          },
+          {
+            "tag": "studiewijzer",
+            "linkText":"ECTS Fiche",
+            "linkUrl": "https://ibamaflexweb.pxl.be/BMFUIDetailxOLOD.aspx?b=1&c=1&a=62329"
+          },
+          {
+            "tag": "studiewijzer",
+            "linkText":"Examenrichtlijnen",
+            "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357648-dt-content-rid-2573247_1/xid-2573247_1"
+          }
+        ]
+      },
+      {
+        "section": "H1 Inleiding Angular",
+        "description": "In dit hoofdstuk krijg je een algemeen beeld van het Angular framework.",
+        "resources": [
+          {
+            "tag": "slides",
+            "linkText":"Inleiding Angular",
+            "linkUrl": "https://docs.google.com/presentation/d/1e5wo_4bmZ21UHwrNH4NnFTqkOb4b7k9k81CJ4YlpO-k/edit?usp=sharing"
+          },
+          {
+            "tag": "pluralsight",
+            "linkText":"Angular Getting started (D. Kurata)",
+            "linkUrl":"https://app.pluralsight.com/course-player?course=angular-2-getting-started-update&author=deborah-kurata&name=angular-2-getting-started-update-m1&clip=0&mode=live"
+          },
+          {
+            "tag": "pluralsight",
+            "linkText":"Introduction (J. Cooper)",
+            "linkUrl":"https://app.pluralsight.com/course-player?course=angular-fundamentals&author=jim-cooper&name=angular-fundamentals-m1&clip=0&mode=live"
+          },
+          {
+            "tag": "extern",
+            "linkText":"Angular getting started guide",
+            "linkUrl": "https://angular.io/start"
+          },
+          {
+            "tag": "extern",
+            "linkText":"Angular documentatie",
+            "linkUrl": "https://angular.io/docs"
+          }
+        ]
+      },
+      {
+        "section": "H2 Typescript",
+        "description": "In dit hoofdstuk leer je werken met typescript, een superset van Javascript die gebruikt wordt binnen de Angular omgeving.",
+        "resources": [
+          {
+            "tag": "slides",
+            "linkText":"Basis typescript",
+            "linkUrl": "https://drive.google.com/open?id=1MU_jtMmMpDj-uXsKcmHzKN_J5lEJXBtYyFwJ7-wIqxw"
+          },
+          {
+            "tag": "extern",
+            "linkText":"TypeScript documentatie",
+            "linkUrl": "https://www.typescriptlang.org/"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 2 - Angular",
+    "content": [
+      {
+        "section": "H3 Werken met components",
+        "description": "In dit hoofdstuk leer je werken met components binnen een component based framework.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Components in Angular",
+              "linkUrl": "https://drive.google.com/open?id=1Mjfla5cdTX-piD48OYIdOJFkXnqPZYcTQtbobfizvYE"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Angular Components (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=c5bdc40d-4824-4b24-8a89-1070915128e5"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Introduction to components (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/course-player?course=angular-2-getting-started-update&author=deborah-kurata&name=angular-2-getting-started-update-m3&clip=0&mode=live"
+            },
+            {
+              "tag": "oefening",
+              "linkText": "Oefeningenbundel Components",
+              "linkUrl": "https://drive.google.com/open?id=1JVc1PGeAQZhz1V5NAjl7-SggQ92ZDQsqfuKbx_yMrwA"
+            },
+            {
+              "tag": "extern",
+              "linkText":"Introduction to components",
+              "linkUrl": "https://angular.io/guide/architecture-components"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 3 - Angular",
+    "content": [
+      {
+        "section": "H4 Databinding & Eventbinding",
+        "description": "In dit hoofdstuk behandelen we de stukken rond databinding in components, communicatie tussen components en eventbinding binnen Angular applicaties.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Databinding & eventbinding",
+              "linkUrl": "https://docs.google.com/presentation/d/1flkVpUdvzGyrdR99-WxY8HN_KUKHt7UjcIs0Zp6A37E/edit?usp=sharing"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "H4 Voorbeeld 1 - Databinding",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH4_Voorbeeld1"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Databinding & eventbinding (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/player?course=angular-2-getting-started-update&author=deborah-kurata&name=angular-2-getting-started-update-m5&clip=0&mode=live"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "component communication (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/course-player?course=angular-fundamentals&author=jim-cooper&name=angular-fundamentals-m2&clip=0&mode=live"
+            },
+            {
+              "tag": "video",
+              "linkText": "Crashcourse databinding Angular",
+              "linkUrl": "https://www.youtube.com/watch?v=RJ3VjuwBM6w"
+            },
+            {
+              "tag": "extern",
+              "linkText": "Component interaction",
+              "linkUrl": "https://angular.io/guide/component-interaction" 
+            }          
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 4 - Angular",
+    "content": [
+      {
+        "section": "H5 attribute directives & property bindings",
+        "description": "In dit hoofdstuk behandelen we de stukken rond attribute directives en structurele directives zoals ngFor en ngIf.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"attribute directives & property bindings",
+              "linkUrl": "https://docs.google.com/presentation/d/16DpnfnnxGzAvdzR7PWyXdaHyyRhXCwSCMcBkEx-xKX0/edit?usp=sharing"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "H5 Voorbeeld 1 - directives",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH5_Voorbeeld1"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Templates & directives (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/player?course=angular-2-getting-started-update&author=deborah-kurata&name=angular-2-getting-started-update-m4&clip=0&mode=live"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Template syntax (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/player?course=angular-fundamentals&author=jim-cooper&name=angular-fundamentals-m3&clip=0&mode=live"
+            },
+            {
+              "tag": "oefening",
+              "linkText": "Oefeningenbundel directives",
+              "linkUrl": "https://docs.google.com/document/d/1lZa-BAMsS4-qIzU68fV4YqIjmoGjEK0zc61V9zuGvCA/edit?usp=sharing"
+            },
+            {
+              "tag": "extern",
+              "linkText": "Structural directives",
+              "linkUrl": "https://angular.io/guide/structural-directives"
+            },
+            {
+              "tag": "bijlage",
+              "linkText": "afbeeldingen oefening pokemon",
+              "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357645-dt-content-rid-2573244_1/xid-2573244_1"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 5 - Angular",
+    "content": [
+      {
+        "section": "H6 Forms",
+        "description": "In dit hoofdstuk leer je werken met template-driven en reactive-forms(model driven) in angular.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Forms, validation and functions",
+              "linkUrl": "https://docs.google.com/presentation/d/1wvz3nzKLAPCENifJmfV6kqTz6d_NKdCMlYB4_SBz3YE/edit?usp=sharing"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "Template driven form",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH6_Voorbeeld1"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "Model driven form",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH6_Voorbeeld2"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Angular Forms & validation (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=891e376c-1a78-4af3-9fd1-0aa77f96bf86"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 6 - Angular",
+    "content": [
+      {
+        "section": "H7 Services and DI",
+        "description": "Hier leren we werken met dependency injection in Angular. Daarnaast behandelen we het ontwerp en gebruik van services.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Services & DI",
+              "linkUrl": "https://docs.google.com/presentation/d/1cCbDQ2N2NUUock-n436x9i-Y5RAatq7A_7zUeuTgxGA/edit?usp=sharing"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "forms and services",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH7_Voorbeeld1"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Services & DI (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=511c7df6-35d6-4a85-a5a0-f83190cdcf34"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Services & DI (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=e4ccfdaa-97ba-4a79-a2bd-fb3a36ec6fbf"
+            },
+            {
+              "tag": "oefening",
+              "linkText": "Oefeningenbundel Services & DI",
+              "linkUrl": "https://docs.google.com/document/d/1iXvhW2fduIngS8HukCjjHbs2Rlc9lepxwC2tbELQoZU/edit?usp=sharing"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 7 - Angular",
+    "content": [
+      {
+        "section": "H8 Data ophalen met HTTP",
+        "description": "We behandelen het gebruik van de HTTPclient in Angular om HTTP request te kunnen uitvoeren. Daarnaast leer je werken met Observables uit de Rxjs library.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"HTTP in Angular",
+              "linkUrl": "https://docs.google.com/presentation/d/1mCFesigLlvEYW_R-fF7exUq9UgqiwLw3XKhUznPrepY/edit?usp=sharing"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "HTTP request lokale json file",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH8_Voorbeeld1"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "Http request aanspreken API",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH8_Voorbeeld2"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Communication with HTTP (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=21db32e5-3fcd-41cf-9701-15a66cad44b9"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Retrieving data using HTTP (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=1405f463-67e1-48b2-9d88-317255375e8a"
+            },
+            {
+              "tag": "oefening",
+              "linkText": "Oefeningenbundel HTTP",
+              "linkUrl": "https://docs.google.com/document/d/1DQkereTKx96J0h9o7he2knmg8lJ2dqD2PlMCIpPVICU/edit?usp=sharing"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 8 - Angular",
+    "content": [
+      {
+        "section": "H8 In memory web API",
+        "description": "In dit onderdeel leer je werken met de in-memory-web-API module die je kan gebruiken om een backend te mocken.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"in memory web API",
+              "linkUrl": "https://drive.google.com/open?id=1aoqDTJ2wNcuw1omyMXZHnorPJ4oDbjfMR3Wf8h5w_QQ"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "In memory web API implementatie",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH9_Voorbeeld1"
+            },
+            {
+              "tag": "video",
+              "linkText": "Inmemory-web-API (Lyrad Digital)",
+              "linkUrl": "https://www.youtube.com/watch?v=0vNcYT6e99c"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 9 & 10 - Angular",
+    "content": [
+      {
+        "section": "H9 Routing",
+        "description": "In dit hoofdstuk voegen we routing toe aan de angular applicatie. Je leert werken met de routing module, routeguards en resolvers.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Routing",
+              "linkUrl": "https://drive.google.com/open?id=1iGGqGuagCdqAb5lj7VkBGa22Wskgbp3epDcHGKCPpC0"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "implementatie routing en routeguards",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH9_Voorbeeld2"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Routing & navigation (J. Cooper)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=a14bec9e-5264-41f4-8d93-8d42b25f9bfd"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Basic Routing (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=2cb48301-c334-41ba-82d7-70dc92c56fe6"
+            },
+            {
+              "tag": "oefening",
+              "linkText": "Oefeningenbundel Routing",
+              "linkUrl": "https://drive.google.com/open?id=1mGN8bVZBcVAappMckluMY-qJy686z6erfzCi8DeEpdY"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 11 - NodeJS",
+    "content": [
+      {
+        "section": "H10 Introductie NodeJS",
+        "description": "In dit hoofdstuk maak je kennis met de NodeJS omgeving. We zetten een eerste applicatie op, je leert werken met modules en bekijken de nodeJS architectuur.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Introductie NodeJS",
+              "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357682-dt-content-rid-2573268_1/xid-2573268_1"
+            },
+            {
+              "tag": "voorbeeld",
+              "linkText": "Voorbeeldenbundel NodeJS",
+              "linkUrl": "https://github.com/PXL-WebExpert-2018/CH10_Voorbeelden"
+            },
+            {
+              "tag": "pluralsight",
+              "linkText": "Basic Routing (D. Kurata)",
+              "linkUrl": "https://app.pluralsight.com/course-player?clipId=2cb48301-c334-41ba-82d7-70dc92c56fe6"
+            }
+        ]
+      }
+    ]
+  },
+  {
+    "contact": "week 12 - NodeJS",
+    "content": [
+      {
+        "section": "H11 NodeJS & Express",
+        "description": "In dit hoofdstuk leer je werken met het Express (middleware) framework. We leren hoe je web applicaties & APIs kan opbouwen in express.",
+        "resources": [
+          {
+            "tag": "slides",
+            "linkText":"Introductie Express",
+            "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357684-dt-content-rid-2573269_1/xid-2573269_1"
+          },
+          {
+            "tag": "voorbeeld",
+            "linkText": "Voorbeeldenbundel NodeJS",
+            "linkUrl": "https://github.com/PXL-WebExpert-2018/CH11_Voorbeelden"
+          },
+          {
+            "tag": "pluralsight",
+            "linkText": "RESTful with NodeJs & Express (J. Mills)",
+            "linkUrl": "https://app.pluralsight.com/library/courses/node-js-express-rest-web-services-update"
+          },
+          {
+            "tag": "oefening",
+            "linkText": "Oefeningenbundel NodeJS & Express",
+            "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357685-dt-content-rid-2573270_1/xid-2573270_1"
+          }
+      ]
+      }
+    ]
+  },
+  {
+    "contact": "week 13 - NodeJS",
+    "content": [
+      {
+        "section": "H12 NodeJS & Mongoose",
+        "description": "In dit hoofdstuk leer je werken ODM mapping via mongoose en MongoDB.",
+        "resources": [
+          {
+            "tag": "slides",
+            "linkText":"MongoDB & Mongoose",
+            "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357690-dt-content-rid-2573271_1/xid-2573271_1"
+          },
+          {
+            "tag": "voorbeeld",
+            "linkText": "Voorbeeldenbundel NodeJS",
+            "linkUrl": "https://github.com/PXL-WebExpert-2018/CH12_Voorbeelden"
+          },
+          {
+            "tag": "pluralsight",
+            "linkText": "Mongoose & Node (M. Scott)",
+            "linkUrl": "https://app.pluralsight.com/library/courses/fundamentals-mongoose-for-node-mongodb"
+          },
+          {
+            "tag": "oefening",
+            "linkText": "Oefeningenbundel NodeJS & Express",
+            "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357693-dt-content-rid-2573273_1/xid-2573273_1"
+          }
+      ]
+      }
+    ]
+  },
+  {
+    "contact": "week 14 - SaSS",
+    "content": [
+      {
+        "section": "H13 CSS Preprocessors",
+        "description": "In dit hoofdstuk leer je SaSs.",
+        "resources": [
+          {
+            "tag": "slides",
+            "linkText":"CSS preprocessors",
+            "linkUrl": "https://blackboard.pxl.be/bbcswebdav/pid-357692-dt-content-rid-2573272_1/xid-2573272_1"
+          },
+          {
+            "tag": "pluralsight",
+            "linkText": "LESS and SASS (S. Wildermuth)",
+            "linkUrl": "https://app.pluralsight.com/library/courses/better-css"
+          }
+      ]
+      }
+    ]
+  }
+]
+`;
+
+
+/*
+{
+    "contact": "week 1 - Angular",
+    "content": [
+      {
+        "section": "H0 Intro web expert",
+        "description": "Dit hoofdstuk geeft een algemeen beeld van het vak Web Expert.",
+        "resources": [
+            {
+              "tag": "slides",
+              "linkText":"Intro web expert",
+              "linkUrl": "https://docs.google.com/presentation/d/1q3SnLGD7DUw7pr_pGHcifu3DoNme_rBGLYsSlNGO5Rs/edit?usp=sharing"
+            }
+        ]
+      }
+    ]
+  }
+  */
